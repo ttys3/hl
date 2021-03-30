47e34763481bb6f7ca10179ac8a391541347181e
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2021-03-30 10:56:02 +0300

    draft

--- src/app.rs
+++ src/app.rs
@@ -295,6 +295,7 @@ impl App {
                 }
                 Ok(())
             }));
+            /*
             // spawn reader threads
             let reader = scope.spawn(closure!(clone sfi, |_| -> Result<()> {
                 let mut workspace = VecDeque::new();
@@ -328,6 +329,7 @@ impl App {
             for reader in readers {
                 reader.join().unwrap()?;
             }
+            */
             writer.join().unwrap()?;
             Ok(())
         })
